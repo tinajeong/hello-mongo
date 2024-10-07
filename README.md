70711aa2eea3f516ce006551e120bcf74a7c54f3
Tina <migujeong@gmail.com>
2024-10-08 02:51:19 +0900

    feat : gradio 추가

---
+++ README.md
@@ -0,0 +1,6 @@
+
+![img.png](static/img.png)
+
+
+### Public interface
+- https://c56e5e3c7e9415174f.gradio.live/
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
+import gradio as gr
 
 app = FastAPI()
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client.test_database
+db.test_collection.insert_one({
+    "question": "Which floor is Lyf Funan gym on?",
+    "answer": "on the 6th floor."
+})
+
+
+def get_answer(question: str):
+    result = db.test_collection.find_one({"question": question})
+    if result:
+        return result["answer"]
+    else:
+        return "Sorry, I don't have an answer for that question."
+
+def gradio_interface(question: str):
+    return get_answer(question)
+
+gr_interface = gr.Interface(fn=gradio_interface, inputs="text", outputs="text", title="MongoDB With Q&A")
 
 @app.get("/")
 def read_root():
-    db.test_collection.insert_one({
-        "question": "Which floor is Lyf Funan gym on?",
-        "answer": "on the 6th floor."
-    })
+    return {"message": "FastAPI with MongoDB!!"}
 
-    result = db.test_collection.find_one({"question": "Which floor is Lyf Funan gym on?"})
-
-    return {
-        "question": result['question'],
-        "answer": result['answer']
-    }
+@app.get("/gradio")
+def gradio_launch():
+    return gr_interface.launch(share=True)
